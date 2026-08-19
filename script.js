2bb7d1c83de9f609356a26eb762c3b09f82c5894
nirmala2507149 <nirmalasom2507@gmail.com>
2026-08-19 11:55:59 +0530

    adding javasript

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-        var a=10
-        var b=20
-        console.log(a+b)
+console.log("javascript is connnected");
+
+
+let year=new Date().getFullYear();
+document.querySelector(".site-footer p").innerHTML=
+`&copy; ${year} Nirmala.All rights reserved`
+
+function getGreeting() {
+    const hour=new Date().getHours();
+    if (hour<12) return"Good Morning";
+    if (hour<17) return"Good Afternoon";
+    return "Good Evening"
+} 
+let hero=document.querySelector(".hero-section h1")
+if(hero){
+    hero.textContent=`${getGreeting()}, I'm Nirmala 👋`
+}
