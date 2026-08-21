45cc3af8885e667e4d9ca725895689c2f2f7a12f
nirmala2507149 <nirmalasom2507@gmail.com>
2026-08-21 11:53:24 +0530

    modified project

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 console.log("javascript is connnected");
 
-
 let year=new Date().getFullYear();
 document.querySelector(".site-footer p").innerHTML=
 `&copy; ${year} Nirmala.All rights reserved`
@@ -16,3 +15,39 @@ let hero=document.querySelector(".hero-section h1")
 if(hero){
     hero.textContent=`${getGreeting()}, I'm Nirmala 👋`
 }
+
+//menu toggle
+let menuaToggle=document.querySelector(".menu-toggle");
+let navLinks=document.querySelector(".nav-links")
+menuaToggle.addEventListener("click",()=>{
+    navLinks.classList.toggle("open");
+    menuaToggle.setAttribute("aria-expanded",
+    navLinks.classList.contains("open")
+    );
+})
+let header=document.querySelector(".site-header")
+window.addEventListener('scroll',()=>{
+    if(window.scrollY>50){
+        header.classList.add('scrolled')
+    }
+    else{
+        header.classList.remove("scrolled")
+    }
+})
+let sections=document.querySelectorAll('section[id]');
+let navitems=document.querySelectorAll(".nav-links a")
+
+window.addEventListener("scroll",()=>{
+    let current="";
+    sections.forEach(section=>{
+        if(window.scrollY>=section.offsetTop-100){
+            current=section.getAttribute("id")
+        }
+    })
+})
+navitems.forEach(link=>{
+    link.classList.remove("active");
+    if(link.getAttribute('href')=='#${current}'){
+        link.classList.add('aactive')
+    }
+})
